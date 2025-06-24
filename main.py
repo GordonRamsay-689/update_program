d5910b164439fe61881f8305a1f92e2a95732608
GordonRamsay-689 <172498648+GordonRamsay-689@users.noreply.github.com>
2025-06-24 10:22:36 +0200

    new

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 
-# comment
 
 path = __file__.strip('main.py')
 print(path)
 
-os.system(f'cd {path}; git pull origin main')
+
+def git():
+    os.system(f'cd {path}; git pull origin main')
+
+def wget():
+    os.system(f"cd {path}; wget -P {path} https://github.com/GordonRamsay-689/update_program/archive/refs/heads/main.zip; unzip {path}/main.zip; mv update_program-main/* ./;rm -r update_program-main")
+
+wget()
