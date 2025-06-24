5e0e23cac854e5a85173e18ce3ee788520697ac7
GordonRamsay-689 <172498648+GordonRamsay-689@users.noreply.github.com>
2025-06-24 10:26:26 +0200

    m

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def git():
     os.system(f'cd {path}; git pull origin main')
 
 def wget():
-    os.system(f"cd {path}; wget -P {path} https://github.com/GordonRamsay-689/update_program/archive/refs/heads/main.zip; unzip {path}/main.zip; mv update_program-main/* ./;rm -r update_program-main")
+    os.system(f"wget -P {path} https://github.com/GordonRamsay-689/update_program/archive/refs/heads/main.zip; unzip {path}/main.zip; mv {path}/update_program-main/* {path}/;rm -r {path}/update_program-main; rm {path}main.zip")
 
 wget()
